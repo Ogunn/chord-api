89bca37b06cc993b51d4e0cf07204efd6b094dd7
Tiago Santos <tiagosn03@gmail.com>
2019-11-05 10:07:54 -0300

    Refactoring: separete the mongodb connection from the main index.ts file.

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,13 @@
-import * as express from "express";
-import * as mongoose from "mongoose";
-import * as bodyParser from "body-parser";
+import express from "express";
+import bodyParser from "body-parser";
 
-import router from "./route/Chart";
-
-mongoose
-  .connect(
-    "mongodb+srv://tiago:tiago@cluster0-1ac6l.mongodb.net/api-chart?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(() => console.log("db connected"))
-  .catch(err => console.log(err));
+import router from "./route";
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/chord", router);
-
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.send("Funcionando");
-});
+app.use("/", router);
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
